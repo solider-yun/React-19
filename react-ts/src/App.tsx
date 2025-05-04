682288e30a3e1e6af47a0d243d529bb80a974d7e
solider-yun <yoongukbae@naver.com>
2025-05-04 05:35:55 +0000

    feat: draggable

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,72 +1,73 @@
-import { EventHandler, useEffect, useState } from 'react'
-import { Apple } from './components/Apple'
+import { useEffect, useState } from "react"
 
-const homeStyle = {
-  width:'1000px',
-  height:'800px'
+
+const Container:React.CSSProperties = {
+  position:'relative',
+  width:"100vw",
+  height:'100vh',
 }
 
-interface dragDrop {
-  startP?:[number,number]
-  currentP?:[number,number]
+const BoxCss:React.CSSProperties = {
+  position:'absolute',
+  width:'10px',
+  height:'10px',
+  background:'blue',
+  transformOrigin:'left top'
 }
 
-const testSet = [
-  [0,1,2,3,4,5,6,7,8,9],
-  [10,11,12,13,14,15,16,17,18,19],
-  [20,21,22,23,24,25,26,27,28,29],
-  [30,31,32,33,34,35,36,37,38,39]
-]
+
 
 function App() {
-  const [drag, setDrag] = useState<dragDrop>();
-  const [isDown, setIsDown] = useState(false);
-  const [target, setTarget] = useState<Array<Array<number>>>(testSet);
+  const [clickPoint,setclickPoint] = useState<{startX?:number,startY?:number}>();
+  const [dragPoint,setdragPoint] = useState<{dragX?:number,dragY?:number}>();
+  const startX = clickPoint?.startX ?? 0;
+  const startY = clickPoint?.startY ?? 0;
+  const dragX = dragPoint?.dragX ?? 0;
+  const dragY = dragPoint?.dragY ?? 0;
 
-  const handleMouseDown:React.MouseEventHandler<HTMLDivElement> = (event) => {
-    console.log('on:',event)
-    setDrag({startP:[event.clientX,event.clientY]})
-    setIsDown(true)
-    return null;
+  const widthCul = dragX !== 0 ? Math.abs(startX - dragX) : 0
+  const heightCul = dragY !== 0 ? Math.abs(startY - dragY) : 0
+
+  const MouseDownEventHandler = (ev: MouseEvent) => {
+    setclickPoint({startX:ev.pageX,startY:ev.pageY})
+    console.log(clickPoint)
   }
 
-  const handleMouseUp:React.MouseEventHandler<HTMLDivElement>  = (event) => {
-    console.log('leave:',event)
-    setDrag({})
-    setIsDown(false)
+  const MouseDragEventHandler = (ev: MouseEvent) => {
+    if(startX && startX !== 0 && startY && startY !== 0){
+      console.log(startX,startY)
+      setdragPoint({dragX:ev.pageX,dragY:ev.pageY})
+    }
   }
 
-  const hanldeMouseMove:React.MouseEventHandler<HTMLDivElement> = (event) => {
-    setDrag({...drag,currentP:[event.clientX,event.clientY]})
+  const MouseUpEventHandler = () => {
+    setclickPoint({startX:0,startY:0})
+    setdragPoint({dragX:0,dragY:0})
   }
 
-  useEffect(() => {
-      
 
-  },[])
+useEffect(() => {
+  window.addEventListener('mousedown',MouseDownEventHandler)
+  window.addEventListener('mousemove',MouseDragEventHandler)
+  window.addEventListener('mouseup',MouseUpEventHandler)
 
+  return () => {
+    window.removeEventListener('mousedown',MouseDownEventHandler);
+    window.removeEventListener('mousemove',MouseDragEventHandler);
+    window.removeEventListener('mouseup',MouseUpEventHandler);
+  }
+},[MouseDownEventHandler,MouseDragEventHandler,MouseUpEventHandler])
+  
   return (
-    <div style={homeStyle} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={hanldeMouseMove}>
-    {/* {
-      target?.map((v,row) => {
-        return (<div style={{
-          display:'flex',
-        }}>{
-        v.map((number,column) => {
-          return <Apple key={`${row}_${column}`}  text={number}/>
-        })}
-        </div>)
-      })
-    } */}
-   {drag && <div style={{
-      position:'relative',
-      top:`${drag?.startP?.[0]}`,
-      left:`${drag?.startP?.[1]}`,
-      width:`${drag?.currentP?.[0]}`,
-      height:`${drag?.currentP?.[1]}`,
-      backgroundColor:'blue'
-    }}></div>}
-    </div>https://oliveyoung.tech/2023-10-20/mouse/
+    <div style={Container}>
+      <div style={{...BoxCss,
+  left:startX,
+  top:startY,
+  width:  widthCul,
+  height: heightCul
+  }}>
+      </div>
+    </div>
   )
 }
 
