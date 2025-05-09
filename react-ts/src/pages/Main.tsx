9e5258bc9ab08b5440e484cad3ea2a07f96cafc4
solider-yun <yoongukbae@naver.com>
2025-05-09 11:20:35 +0000

    add: some line in main

--- react-ts/src/pages/Main.tsx
+++ react-ts/src/pages/Main.tsx
@@ -2,17 +2,28 @@ import { useState } from "react";
 import Meteor from "../components/MeteorSky";
 import Ground from "../components/Ground";
 import Container from "../components/Container";
+import Typography from "../components/Typography";
+import Button from "../components/Button";
 
 const Main = () => {
   const [run, setRun] = useState(false);
-  const handleRun = () => {
-    setRun(!run);
-  };
 
   return (
     <Container trigger={run}>
-      <div>
-        
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h1">Applecalypse</Typography>
+        <Typography variant="body">Apple + Apocalypse</Typography>
+        <div style={{ height: "70px" }}></div>
+        <Button variant="secondary">Drop the Apple!</Button>
       </div>
     </Container>
   );
